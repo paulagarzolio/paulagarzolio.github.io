6ffda34ba0e1c7c24eb431751b3dbeda02e0fda7
paulagarzolio <paula.garzolio01@estudiant.upf.edu>
2022-04-05 22:54:19 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -42,7 +42,8 @@ function initSecondScene(){
 	query("#FirstScene").style.display="none";
 	var sceneDiv= query("#SecondScene");
 	sceneDiv.style.display="";
-	var messages = ["Bienvenido/a","Antes de nada, elige a tu personaje para empezar."];
+	var messages = "Bienvenido/a;Hola!;Que hago yo aquí?;$No te preocupes, pronto vas a entenderlo todo. Antes de nada, elige a tu personaje para empezar.$Verás, desde hace unos años las redes sociales se han convertido en un lugar cada vez más solitario y cruel con unos estandares de belleza con los que muchos usarios no se sienten identificados. La imagen corporal de muchos de ellos, ha quedado afectada por estos estandares y los usuarios se sienten cada vez más inseguros con sus cuerpos.;\
+	Qué es la Imagen Corporal?;Entiendo...;$La imagen corporal es la idea que cada uno tiene de su propia aspecto físico y de cómo le ven los demás.";
 	Dialogue.init(messages);
 	
 }
@@ -81,28 +82,52 @@ function initSecondGame(){
 }
 var avatar;
 var Dialogue = {
-    contents:[],
 	messages:[],
     actualMessage:-1,
     div:"",
 
-    init: function(messages){
-        this.div =query("#SecondScene");
-        this.actualMessage=0;
-        this.contents=messages;
-        setTimeout(this.nextMessage,500);
+    init: function(txt){
+        this.div =query("#story");
+		var messages =txt.split(["$"]);
+		for(var i=0; i<messages.length;i++){
+			var strings = messages[i].split([";"]);
+			var question = strings[0];
+			var answers = strings.splice(1,2);
+			var message =new Message(i,question,answers);
+			this.messages.push(message);
+		} 
+		this.actualMessage=0;
+		setTimeout(this.nextMessage,500);
+		
         
     },
     setMessages: function(){
 		var that= Dialogue;
-		var content = that.contents[that.actualMessage]
-		var message = new Message(that.actualMessage,content);
-		that.messages.push(message);
+		var message = that.messages[that.actualMessage];
 		var id = ("#info"+that.actualMessage).toString();
-		var div=query(".info").cloneNode();
-		div.id = id
-		message.div= div;
-		message.div.innerHTML = message.content;
+		if (message.answers.length>0){
+			var div=query(".templates .info").cloneNode(true);
+			message.div=div;
+			var answer0 = message.div.children[1];
+			var answer1=message.div.children[2];
+			answer0.innerHTML=message.answers[0];
+			answer1.innerHTML=message.answers[1];
+			answer0.addEventListener("click",Dialogue.nextMessage);
+			answer1.addEventListener("click",Dialogue.nextMessage);
+		}
+		else {
+			var div=query(".info").cloneNode();
+			var quest =query(".Info").cloneNode();
+			div.appendChild(quest);
+			message.div=div;
+			setTimeout(this.showAvatars,100);
+		}
+		message.div.id = id
+		message.div.children[0].innerHTML=message.question;
+		that.div.appendChild(message.div);
+		unfade(that.div);
+		that.div.scrollTop=10000;
+		message.div.style.opacity="1";
 		return message
         
     },
@@ -121,36 +146,57 @@ var Dialogue = {
 	},
 	chooseAvatar: function(){
 		avatar = Dialogue.findSrc(this.children[0].src);
-		initFirstGame();
+		this.style.backgroundColor="rgb(0, 74, 173)";
+		this.actualMessage=2;
+		setTimeout(Dialogue.nextMessage,500);
+		
+		//initFirstGame();
 	},
 	showAvatars: function(){
 		var avatars=query("#avatars");
-		query("#SecondScene").appendChild(avatars);
+		query("#story").children[1].appendChild(avatars);
 		avatars.style.display="";
 		avatars.children[0].addEventListener("click",Dialogue.chooseAvatar);
 		avatars.children[1].addEventListener("click",Dialogue.chooseAvatar);
 		avatars.children[2].addEventListener("click",Dialogue.chooseAvatar);
+		query("#story").scrollTop=10000;
 	},
 	nextMessage: function(){
 		var that= Dialogue;
+		var prev= that.actualMessage-1;
 		if(that.actualMessage-1>=0){
-			var prev= that.actualMessage-1;
-			var prev_message=query("#SecondScene").children[prev+1];
-			prev_message.style.color="rgba(28, 28, 30, 0.337)";
+			var prev_message=query("#story").children[prev];
+			if(that.messages[prev].answers.length>0){
+				prev_message.children[2].remove();
+				prev_message.children[1].remove();
+			}
+			prev_message.style.opacity="0.6";
+
 		}
+		else if (prev>=0 && that.messages[prev].answers.length==0) query("#story").children[prev].children[1].remove();
 		var message = that.setMessages();
-		that.div.appendChild(message.div);
-		message.div.style.color="rgba(28, 28, 30)";
 		++that.actualMessage;
-		if(that.actualMessage<that.contents.length) setTimeout(that.nextMessage,1500);
-		else setTimeout(that.showAvatars,1500);
+		
+	
 	}
 
 }
-
-function Message(id,content){
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+function Message(id,question,answers=[]){
 	this.id=id;
-	this.content=content;
+	this.question=question;
+	this.answers=answers;
     this.div="";
 }
 
